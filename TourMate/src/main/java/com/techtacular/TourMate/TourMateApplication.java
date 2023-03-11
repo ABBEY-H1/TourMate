aee3fdc0170c2fd96436421a0dad72ab5df402c3
ABBEY-H1 <98225132+ABBEY-H1@users.noreply.github.com>
2023-03-11 16:25:14 +0530

    first commit

---
+++ TourMate/src/main/java/com/techtacular/TourMate/TourMateApplication.java
@@ -0,0 +1,13 @@
+package com.techtacular.TourMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourMateApplication.class, args);
+	}
+
+}
---
+++ TourMate/src/test/java/com/techtacular/TourMate/TourMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtacular.TourMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
